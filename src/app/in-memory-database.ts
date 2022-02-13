c766361c8c107d2ea4a25e29611b3f352154b637
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-02-13 13:03:40 -0300

    add InMemoryDatabase

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,16 @@
+import { InMemoryDbService, RequestInfo } from "angular-in-memory-web-api";
+import { Observable } from "rxjs";
+
+export class InMemoryDatabase implements InMemoryDbService{
+    createDb(){
+        const categories = [
+            { id: 1, nome: 'Moradia', descricao: 'Pagamentos de contas da casa' },
+            { id: 2, nome: 'Saúde', descricao: 'Planos de saúde e remédios' },
+            { id: 3, nome: 'Lazer', descricao: 'Cinema, parques, praia, etc' },
+            { id: 4, nome: 'Salário', descricao: 'Recebimento de Salário' },
+            { id: 5, nome: 'Freelas', descricao: 'Trabalhos como freelancer' }
+        ];
+
+        return { categories } 
+    }
+}
