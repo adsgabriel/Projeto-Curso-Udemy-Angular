56501faa25ae186e29f5aae9ff4945afb2509918
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-02-20 09:46:32 -0300

    add novos recurso entry service

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Injector } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
+import { mergeMap, catchError, map } from 'rxjs/operators';
 
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 import { CategoryService } from '../../categories/shared/category.service';
@@ -14,41 +14,25 @@ import { Entry } from './entry.model';
 export class EntryService extends BaseResourceService<Entry> {
 
   constructor(protected injetor: Injector, private categoryService: CategoryService) {
-    super("api/entries", injetor);
+    super("api/entries", injetor, Entry.fromJson);
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      mergeMap(category => {
-        entry.category = category;
-       return super.create(entry)
-      })
-    )
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this))
   }
 
   update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this))
+  }
+
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<any>{
     return this.categoryService.getById(entry.categoryId).pipe(
       mergeMap(category => {
         entry.category = category;
-       return super.update(entry)
-      })
-    )
+       return sendFn(entry);
+      }),
+      catchError(this.handleError)
+    );
   }
-
-  //Metodos Privados
-
-  protected jsonDataToResources(jsonData: any[]): Entry[] {
-    const entries: Entry[] = [];
-
-    jsonData.forEach(element => {
-      const entry = Entry.fromJson(element);
-      entries.push(entry);
-    });
-    return entries;
-  }
-
-  protected jsonDataToResource(jsonData: any): Entry {
-    return Entry.fromJson(jsonData);
-  }
-
 }
--- src/app/shared/services/base-resource.service.ts
+++ src/app/shared/services/base-resource.service.ts
@@ -10,45 +10,51 @@ export abstract class BaseResourceService<T extends BaseResourceModel>{
     
     protected http: HttpClient;
 
-    constructor(protected apiPath: string, protected injector: Injector){
+    constructor(
+      protected apiPath: string, 
+      protected injector: Injector, 
+      protected jsonDataToResourceFn: (jsonData: any) => T
+    ){
         this.http = injector.get(HttpClient);
     }
 
     getAll(): Observable<T[]> {
         return this.http.get(this.apiPath).pipe(
+          /* Verificar erro no map antes do catchError*/
           catchError(this.handleError),
-          map(this.jsonDataToResources)
+          map(this.jsonDataToResources.bind(this))  
         )
       }
     
       getById(id: number): Observable<T>{
         const url = `${this.apiPath}/${id}`;
         return this.http.get(url).pipe(
-          catchError(this.handleError),
-          map(this.jsonDataToResource)
+          map(this.jsonDataToResource.bind(this)),
+          catchError(this.handleError)
         )
       }
     
       create(resource: T): Observable<T> {
         return this.http.post(this.apiPath, resource).pipe(
-          catchError(this.handleError),
-          map(this.jsonDataToResource)
+          map(this.jsonDataToResource.bind(this)),
+          catchError(this.handleError)
         )
       }
     
       update(resource: T): Observable<T>{
         const url = `${this.apiPath}/${resource.id}`;
         return this.http.put(url, resource).pipe(
-          catchError(this.handleError),
-          map(() => resource)
+          map(() => resource),
+          catchError(this.handleError) 
         )
       }
     
       delete(id: number): Observable<any> {
         const url = `${this.apiPath}/${id}`;
         return this.http.delete(url).pipe(
-          catchError(this.handleError),
-          map(() => null)
+          map(() => null),
+          catchError(this.handleError)
+          
         )
       }
 
@@ -56,12 +62,12 @@ export abstract class BaseResourceService<T extends BaseResourceModel>{
 
       protected jsonDataToResources(jsonData: any[]): T[]{
         const resources: T[] = [];
-       jsonData.forEach(element => resources.push(element as T));
+       jsonData.forEach(element => resources.push( this.jsonDataToResourceFn(element)));
        return resources;
      }
    
      protected jsonDataToResource(jsonData: any): T{
-       return jsonData as T;
+       return this.jsonDataToResourceFn(jsonData);
      }
    
      protected handleError(error: any): Observable<any>{
