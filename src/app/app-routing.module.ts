acba76b1336d500f5176b351a65ab0081cff23e0
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-02-26 10:05:28 -0300

    add report

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: 'entries', loadChildren: () => import('./pages/entries/entries.module').then(m =>m.EntriesModule)},
-  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) }
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) },
+  { path: 'reports', loadChildren: () => import('./pages/reports/reports.module').then(m => m.ReportsModule) }
 ];
 
 @NgModule({
